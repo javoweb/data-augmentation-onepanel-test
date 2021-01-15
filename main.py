5b45967ebc39678eae35d0adbd4f1f5cb87777e9
Gonzalo Tixilima <javoweb@msn.com>
2021-01-15 07:05:00 -0500

    add train split and data augmentation workflow

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+from val_split import split_dataset
+from data_augmentation import data_augmentation
+from utils import export_dataset
+
+            
+def main(args: argparse.Namespace) -> int:
+
+    train_set, val_set = split_dataset(
+        dataset_name=args.annotations_filename, 
+        val_split=args.val_split, 
+        input_path=args.input_folder, 
+        output_path=args.output_folder
+    )
+
+    train_set = data_augmentation(
+        args.data_aug_params, 
+        train_set, 
+        data_folder=args.output_folder+'train/'
+    )
+
+    export_dataset(args.format, args.output_folder)
+    
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Data splitting and augmentation pipeline')
+    parser.add_argument('--input_folder', default='/mnt/data/')
+    parser.add_argument('--output_folder', default='/mnt/output/')
+    parser.add_argument('--annotations_filename', default='instances_default.json')
+    parser.add_argument('--val_split', default=0.2, type=float)
+    parser.add_argument('--data_aug_params', default='')
+    parser.add_argument('--format', default=None)
+    args = parser.parse_args()
+    main(args)
